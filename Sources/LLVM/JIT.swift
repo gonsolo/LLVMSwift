d5ad30a3177e59b2d6c391e02e9a965d5bc38921
Gon Solo <gonsolo@gmail.com>
2025-03-12 17:20:48 +0100

    bla.

--- Sources/LLVM/JIT.swift
+++ Sources/LLVM/JIT.swift
@@ -4,7 +4,7 @@
 
 public final class JIT {
 
-  let jit = UnsafeMutablePointer<LLVMOrcLLJITRef?>(bitPattern: 0)
+  let jit: UnsafeMutablePointer<LLVMOrcLLJITRef?>? = nil
   let mainDyLib: LLVMOrcJITDylibRef
 
   public init() {
